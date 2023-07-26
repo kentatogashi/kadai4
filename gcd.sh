d1fd338f0a6a1098105b66b90d16dda0a9445d01
togashik <togashik@example.com>
2023-07-26 03:02:12 +0000

    Add error handling on passing arguments

--- gcd.sh
+++ gcd.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
+#!/usr/bin/bash
 
-# Define a function to calculate the GCD
+err_exit() {
+    echo [ERROR] $1 1>&2
+    exit 1
+}
+
 gcd() {
     if [ $2 -eq 0 ]; then
         echo $1
@@ -8,5 +12,13 @@ gcd() {
         gcd $2 $(($1 % $2))
     fi
 }
+
+if [ $# -ne 2 ]; then
+    err_exit "The number of arguments are invalid"
+fi
+
+if ! [[ "$1" =~ ^[0-9]+$ && "$2" =~ ^[0-9]+$ ]] || [[ $1 -eq 0 || $2 -eq 0 ]]; then
+    err_exit "The arguments aren't natural integer"
+fi
 
 gcd $1 $2
